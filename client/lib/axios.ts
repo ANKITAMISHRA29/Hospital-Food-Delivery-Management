447eb8dbf479ace49b61beaf3064da42d27464b8
ANKITA MISHRA <114464293+ANKITAMISHRA29@users.noreply.github.com>
2025-01-11 13:12:30 +0530

    first commit

--- client/lib/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
-  withCredentials: true, // This ensures cookies are sent with every request
-});
-
-export default instance;
